7b724522de06a1ee2f351d2c92e4f940a4866d01
roman-kreichman <roman.kreichman@gmail.com>
2016-09-12 21:49:15 +0300

    TR-1172 - MetadataUtils Update - Added method getFields - Added method getValues - Added method getValuesByXsd - Added method getXmlFromValues

--- sources/pojo/src/main/java/com/kaltura/client/utils/MetadataUtils.java
+++ sources/pojo/src/main/java/com/kaltura/client/utils/MetadataUtils.java
@@ -3,12 +3,18 @@ package com.kaltura.client.utils;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -22,6 +28,7 @@ import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import com.kaltura.client.KalturaApiException;
@@ -93,6 +100,76 @@ public class MetadataUtils {
 
 		return (String) compiledExpression.evaluate(doc, XPathConstants.STRING);
 	}
+	
+	public static List<String> getFields(String xsd) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException
+	{
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		docBuilderFactory.setIgnoringElementContentWhitespace(true);
+
+		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(new ByteArrayInputStream(xsd.getBytes()));
+
+		NodeList nodeList = doc.getElementsByTagName("xsd:element");
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Element element = (Element)nodeList.item(i);
+			String fieldName = element.getAttribute("name");
+			if (fieldName.equals("metadata"))
+				continue;
+			
+			list.add(fieldName);
+		}
+		
+		return list;
+		
+	}
+	
+	public static LinkedHashMap<String, ArrayList<String>> getValues(String xml) throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
+	{
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+		docBuilderFactory.setIgnoringElementContentWhitespace(true);
+
+		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(new ByteArrayInputStream(xml.getBytes()));
+
+		LinkedHashMap<String, ArrayList<String>> values = new LinkedHashMap<String, ArrayList<String>>();
+		NodeList nodeList = doc.getElementsByTagName("metadata");
+		if (nodeList.getLength() > 0) {
+			Element metadataElement = (Element)nodeList.item(0);
+			NodeList metadataNodes = metadataElement.getChildNodes();
+			for (int i = 0; i < metadataNodes.getLength(); i++) {
+				Node tempNode = metadataNodes.item(i);
+				if (!(tempNode instanceof Element))
+					continue;
+				String metadataFieldName = tempNode.getNodeName();
+				String metadataFieldValue = tempNode.getTextContent();
+				
+				if (!values.containsKey(metadataFieldName))
+					values.put(metadataFieldName, new ArrayList<String>());
+				
+				values.get(metadataFieldName).add(metadataFieldValue);
+			}
+		
+		}
+		return values;
+	}
+	
+	public static LinkedHashMap<String, ArrayList<String>> getValuesByXsd(String xml, String xsd) throws XPathExpressionException, ParserConfigurationException, SAXException, IOException
+	{
+		List<String> fields = getFields(xsd);
+		LinkedHashMap<String, ArrayList<String>> values = getValues(xml);
+		
+		LinkedHashMap<String, ArrayList<String>> valuesFromXsd = new LinkedHashMap<String, ArrayList<String>>();
+		for (int i = 0; i < fields.size(); i++) {
+			String field = fields.get(i);
+			if (values.containsKey(field))
+				valuesFromXsd.put(field, values.get(field));
+			else
+				valuesFromXsd.put(field, null);
+		}
+			
+		return valuesFromXsd;
+	}
 
 	public static void setValue(KalturaClient client, String objectId, KalturaMetadataObjectType objectType, String profileSystemName, String xPath, String value) throws XPathExpressionException, ParserConfigurationException, SAXException, IOException, TransformerException, KalturaApiException, MetadataUtilsFieldNotSetException {
 		KalturaMetadataProfile profile = getProfile(client, objectType, profileSystemName);
@@ -126,14 +203,36 @@ public class MetadataUtils {
 		Node node = (Node) compiledExpression.evaluate(doc, XPathConstants.NODE);
 		node.setTextContent(value);
 
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-		StringWriter writer = new StringWriter();
-		transformer.transform(new DOMSource(doc), new StreamResult(writer));
-		String xml = writer.getBuffer().toString().replaceAll("\n|\r", "");
+		String xml = documentToString(doc);
 
 		metadata = client.getMetadataService().update(metadata.getId(), xml);
+	}
+	
+	public static String getXmlFromValues(LinkedHashMap<String, ArrayList<String>> values) throws ParserConfigurationException, TransformerException
+	{
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+		Document doc = docBuilder.newDocument();
+		Element metadataElement = doc.createElement("metadata");
+		doc.appendChild(metadataElement);
+		
+		for (String field : values.keySet())
+		{
+			ArrayList<String> fieldValues = values.get(field);
+			if (fieldValues == null)
+				continue;
+		
+			for(String fieldValue : fieldValues)
+			{
+				Element fieldElement = doc.createElement(field);
+				fieldElement.setTextContent(fieldValue);
+				metadataElement.appendChild(fieldElement);
+			}
+		}
+		
+		String xml = documentToString(doc);
+		return xml;
 	}
 
 	public static KalturaMetadata addMetadata(KalturaClient client, String objectId, KalturaMetadataObjectType objectType, String profileSystemName, String xPath, String value) throws ParserConfigurationException, TransformerException, KalturaApiException {
@@ -159,12 +258,7 @@ public class MetadataUtils {
 		}
 		parentElement.setTextContent(value);
 
-		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-		Transformer transformer = transformerFactory.newTransformer();
-		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-		StringWriter writer = new StringWriter();
-		transformer.transform(new DOMSource(doc), new StreamResult(writer));
-		String xml = writer.getBuffer().toString().replaceAll("\n|\r", "");
+		String xml = documentToString(doc);
 		
 		return addMetadata(client, objectId, objectType, profileId, xml);
 	}
@@ -239,4 +333,14 @@ public class MetadataUtils {
 		return null;
 	}
 
+	private static String documentToString(Document doc) throws TransformerException
+	{
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		StringWriter writer = new StringWriter();
+		transformer.transform(new DOMSource(doc), new StreamResult(writer));
+		String xml = writer.getBuffer().toString().replaceAll("\n|\r", "");
+		return xml;
+	}
 }
