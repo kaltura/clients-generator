25bcafba42e3a4534292f3951efae47305cbc27a
Carlos Corbacho <carlos@strangeworlds.co.uk>
2017-01-20 00:16:05 +0000

    Make configparser import Python 3 compatible
    The configparser module has been renamed in Python 3 - use six for
    compatibility on Python 2 & 3.

--- sources/python/setup.py
+++ sources/python/setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     url='http://www.kaltura.com/api_v3/testme/client-libs.php',
     packages=['KalturaClient', 'KalturaClient.Plugins'],
-    install_requires=['requests', 'requests-toolbelt'],
+    install_requires=['requests', 'requests-toolbelt', 'six'],
     license='AGPL',
     description='A Python module for accessing the Kaltura API.',
     long_description=open('README.txt').read(),
