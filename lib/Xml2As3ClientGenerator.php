367ab31c5057503a669a2e633c75e649c9ac9b23
atarsh <atar.shadmi@kaltura.com>
2017-08-31 13:42:46 +0300

    use double quotes around \n for numbers

--- lib/Xml2As3ClientGenerator.php
+++ lib/Xml2As3ClientGenerator.php
@@ -67,7 +67,7 @@ class Xml2As3ClientGenerator extends ClientGeneratorFromXml
 		{
 			switch($enumType)
 			{
-				case "int" : $str .= '		public static const ' . $child->attributes()->name . ' : int = ' . $child->attributes()->value . ';\n'; break;
+				case "int" : $str .= '		public static const ' . $child->attributes()->name . ' : int = ' . $child->attributes()->value . ";\n"; break;
 				case "string" : $str .= '		public static const ' . $child->attributes()->name . ' : String = "' . $child->attributes()->value . '";' . "\n"; break;
 			}
 		}
