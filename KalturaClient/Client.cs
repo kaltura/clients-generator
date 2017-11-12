afd89b8a223eaaa6237a67b15709b69172dae829
Jenkins <jenkins@kaltura.com>
2017-11-12 15:06:14 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,7 +35,7 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "4.6.39.24011";
+				ApiVersion = "4.6.39.27137";
 				ClientTag = "dotnet:17-11-12";
 		}
 	
