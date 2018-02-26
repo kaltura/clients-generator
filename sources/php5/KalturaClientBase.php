9aaae4fbb6ee38c6f9152a2012d91ec499814a2d
Jess Portnoy <jess.portnoy@kaltura.com>
2018-02-26 17:03:18 +0000

    same for the php5 client and ZF client.

--- sources/php5/KalturaClientBase.php
+++ sources/php5/KalturaClientBase.php
@@ -1066,13 +1066,30 @@ class KalturaClientBase
 
 	protected static function aesEncrypt($key, $message)
 	{
-		return mcrypt_encrypt(
-			MCRYPT_RIJNDAEL_128,
-			substr(sha1($key, true), 0, 16),
-			$message,
-			MCRYPT_MODE_CBC,
-			str_repeat("\0", 16)	// no need for an IV since we add a random string to the message anyway
-		);
+		$iv = str_repeat("\0", 16);    // no need for an IV since we add a random string to the message anyway
+		$key = substr(sha1($key, true), 0, 16);
+		if (function_exists('mcrypt_encrypt')) {
+			return mcrypt_encrypt(
+				MCRYPT_RIJNDAEL_128,
+				$key,
+				$message,
+				MCRYPT_MODE_CBC,
+				$iv
+			);
+		}else {
+			// Pad with null byte to be compatible with mcrypt PKCS#5 padding
+		        // See http://thefsb.tumblr.com/post/110749271235/using-opensslendecrypt-in-php-instead-of as reference
+			$blockSize = 16;
+			$padLength = $blockSize - strlen($message) % $blockSize;
+			$message .= str_repeat(chr(0), $padLength);
+			return openssl_encrypt(
+				$message,
+				'AES-128-CBC',
+				$key,
+				OPENSSL_RAW_DATA | OPENSSL_ZERO_PADDING,
+				$iv
+			);
+		}
 	}
 
 	private function hash ( $salt , $str )
