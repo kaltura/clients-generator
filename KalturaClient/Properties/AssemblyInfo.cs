ba10c5fe1b5b7f54fa4c1198899d613b7761ff52
Jenkins <jenkins@kaltura.com>
2018-03-25 11:12:36 +0000

    Auto-generated by Jenkins

--- KalturaClient/Properties/AssemblyInfo.cs
+++ KalturaClient/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.72.71.15798")]
-[assembly: AssemblyFileVersion("4.72.71.15798")]
+[assembly: AssemblyVersion("4.72.71.20118")]
+[assembly: AssemblyFileVersion("4.72.71.20118")]
