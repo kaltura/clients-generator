c56dfc7f85b777486defcf3d82b829248cc4b1c4
Jenkins <jenkins@kaltura.com>
2018-04-16 10:22:15 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,7 +35,7 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "4.8.57.15936";
+				ApiVersion = "4.8.57.18606";
 				ClientTag = "dotnet:18-04-16";
 		}
 	
--- KalturaClient/Properties/AssemblyInfo.cs
+++ KalturaClient/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.8.57.15936")]
-[assembly: AssemblyFileVersion("4.8.57.15936")]
+[assembly: AssemblyVersion("4.8.57.18606")]
+[assembly: AssemblyFileVersion("4.8.57.18606")]
