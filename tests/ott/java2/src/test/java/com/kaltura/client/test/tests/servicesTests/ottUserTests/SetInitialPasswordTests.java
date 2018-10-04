38e65c7058022334ca3586ba15417f1363e8ce7a
jonathan.amit-kanarek <jonathan.amit-kanarek@kaltura.com>
2018-10-04 16:19:54 +0300

    Removed ott tests

--- tests/ott/java2/src/test/java/com/kaltura/client/test/tests/servicesTests/ottUserTests/SetInitialPasswordTests.java
+++
@@ -1,8 +0,0 @@
-package com.kaltura.client.test.tests.servicesTests.ottUserTests;
-
-import com.kaltura.client.test.tests.BaseTest;
-
-public class SetInitialPasswordTests extends BaseTest {
-
-    // TODO: 4/1/2018 implemente tests  
-}
