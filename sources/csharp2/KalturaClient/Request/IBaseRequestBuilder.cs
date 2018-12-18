8ab3bd7b1519519d9d3117576a9dd2c000c1f8dd
Arthur Vaverko <Arthur.Vaverko@kaltura.com>
2018-12-18 10:23:44 +0200

    fix overriden merge chanages

--- sources/csharp2/KalturaClient/Request/IBaseRequestBuilder.cs
+++ sources/csharp2/KalturaClient/Request/IBaseRequestBuilder.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Xml;
+using Newtonsoft.Json.Linq;
 
 namespace Kaltura.Request
 {
     public interface IBaseRequestBuilder
     {
-        object Deserialize(XmlElement results);
-        object DeserializeObject(object results);
+        object Deserialize(JToken results);
     }
 }
