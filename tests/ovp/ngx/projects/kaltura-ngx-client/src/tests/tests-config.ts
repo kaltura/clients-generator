1bdb29a51e17b0c46c1a1f37fc191b4afb819188
Eran Sakal <eran@sakalim.com>
2019-02-24 10:55:48 +0200

    remove test partner data

--- tests/ovp/ngx/projects/kaltura-ngx-client/src/tests/tests-config.ts
+++
@@ -1,7 +0,0 @@
-export const TestsConfig = {
-	partnerId: "2052371",
-	userName: "eran.sakal@kaltura.com",
-	adminSecret: "1ce1010dac6c92cf4162d981a3385239",
-	endpointUrl: "https://www.kaltura.com",
-	clientTag: "ngx-tests"
-};
