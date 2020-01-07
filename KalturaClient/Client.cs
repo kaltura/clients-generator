9d36d997ef9b76755b82a2dec523dc600ca16587
Backend CI <ott.rnd.core@kaltura.com>
2020-01-07 13:29:20 +0000

    Auto-generated by Jenkins

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2019  Kaltura Inc.
+// Copyright (C) 2006-2020  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -35,8 +35,8 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "5.3.0.14370";
-				ClientTag = "dotnet:20-01-06";
+				ApiVersion = "5.3.0.14371";
+				ClientTag = "dotnet:20-01-07";
 		}
 	
 		#region Properties
