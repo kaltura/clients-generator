d8aed2cade42beed509cdb1ad53f8946cfcfb221
Backend CI <ott.rnd.core@kaltura.com>
2021-01-31 13:34:53 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/348, branch 620_BEO-9373_OPC_Reset_Password_Parameters

--- KalturaClient/APIException.cs
+++ KalturaClient/APIException.cs
@@ -100,6 +100,7 @@ namespace Kaltura
 		public static string ParentIdShouldNotPointToItself = "2041";
 		public static string ParentIdNotExist = "2059";
 		public static string UserFavoriteNotDeleted = "2060";
+		public static string UserSelfDeleteNotPermitted = "2062";
 		public static string InvalidPurchase = "3000";
 		public static string CancelationWindowPeriodExpired = "3001";
 		public static string SubscriptionNotRenewable = "3002";
--- KalturaClient/Enums/PartnerConfigurationType.cs
+++ KalturaClient/Enums/PartnerConfigurationType.cs
@@ -40,6 +40,7 @@ namespace Kaltura.Enums
 		public static readonly PartnerConfigurationType PAYMENT = new PartnerConfigurationType("Payment");
 		public static readonly PartnerConfigurationType CATALOG = new PartnerConfigurationType("Catalog");
 		public static readonly PartnerConfigurationType SECURITY = new PartnerConfigurationType("Security");
+		public static readonly PartnerConfigurationType OPC = new PartnerConfigurationType("Opc");
 
 		private PartnerConfigurationType(string name) : base(name) { }
 	}
