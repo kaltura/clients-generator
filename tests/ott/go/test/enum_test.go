b60b0554a79c10b0a61e5866663e3c85c4f9fd93
shir.bruchim <shir.bruchim@kaltura.com>
2021-04-08 17:26:28 +0300

    enum and error tests

--- tests/ott/go/test/enum_test.go
+++ tests/ott/go/test/enum_test.go
@@ -3,15 +3,17 @@ package test
 import (
 	"encoding/json"
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/epgorderby"
-	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/ruleactiontype"
+	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/userroleprofile"
+	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/userroletype"
 	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/types"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSerializationWithoutValues(t *testing.T) {
+func TestSerializationNoValues(t *testing.T) {
 	t.Parallel()
 
 	var testCases = []SomeStruct{
@@ -26,35 +28,33 @@ func TestSerializationWithoutValues(t *testing.T) {
 			after := SomeStruct{}
 			err = json.Unmarshal(bytes, &after)
 			assert.NoError(t, err)
-
 			assert.Equal(t, before, after)
 		})
 	}
 }
 
-func TestSerializationOfValues(t *testing.T) {
+func TestSerializationValues(t *testing.T) {
 	t.Parallel()
-	order := ruleactiontype.START_DATE_OFFSET
+	customType := userroletype.CUSTOM
+	userroleprofileType := userroleprofile.PARTNER
 
-	var testCases = []types.AccessControlBlockAction{
-		{Type: &order},
+	var testCases = []types.UserRole{
+		{Type: &customType, Profile: userroleprofileType},
+		{Type: nil},
 	}
 	for _, before := range testCases {
 		t.Run(fmt.Sprintf("order: %v", before.Type), func(t *testing.T) {
 			bytes, err := json.Marshal(before)
 			assert.NoError(t, err)
-			t.Log(string(bytes))
-
-			after := types.AccessControlBlockAction{}
+			after := types.UserRole{}
 			err = json.Unmarshal(bytes, &after)
 			assert.NoError(t, err)
-
 			assert.Equal(t, before, after)
 		})
 	}
 }
 
-func TestDeserialization(t *testing.T) {
+func TestDeserializationNoValues(t *testing.T) {
 	t.Parallel()
 
 	deserialized := SomeStruct{}
@@ -66,10 +66,53 @@ func TestDeserialization(t *testing.T) {
 	assert.Equal(t, SomeStruct{}, deserialized)
 }
 
-// epgcache
-// type SomeStruct struct {
-// 	OrderBy *epgorderby.EpgOrderBy`json:"orderBy,omitempty"`
-// }
+func TestDeserializationValues(t *testing.T) {
+	t.Parallel()
+	space := regexp.MustCompile(`\s+`)
+	deserialized := types.UserRole{}
+	userRoleEmptyString := space.ReplaceAllString(GetUserRoleEmptyString(), "")
+	err := json.Unmarshal([]byte(userRoleEmptyString), &deserialized)
+	assert.Error(t, err)
+
+	userRoleNullString := space.ReplaceAllString(GetUserRoleNullString(), "")
+	err = json.Unmarshal([]byte(userRoleNullString), &deserialized)
+	assert.NoError(t, err)
+	assert.Equal(t, types.UserRole{Profile: userroleprofile.USER}, deserialized)
+
+	userRoleValuesString := space.ReplaceAllString(GetUserRoleValuesString(), "")
+	err = json.Unmarshal([]byte(userRoleValuesString), &deserialized)
+	assert.NoError(t, err)
+	userroleType := userroletype.CUSTOM
+	assert.Equal(t, types.UserRole{Profile: userroleprofile.PARTNER,
+		Type: &userroleType,
+	}, deserialized)
+}
+
+// TODO USE types.EpgCache instead of SomeStruct (need to delete SomeStruct)
 type SomeStruct struct {
 	OrderBy *epgorderby.EpgOrderBy `json:"orderBy,omitempty"`
+}
+
+func GetUserRoleEmptyString() string {
+	return `{
+		"type": "",
+		"profile": "",
+		"objectType": "KalturaUserRole"
+	  }`
+}
+
+func GetUserRoleNullString() string {
+	return `{
+		"type": null,
+		"profile": "USER",
+		"objectType": "KalturaUserRole"
+	  }`
+}
+
+func GetUserRoleValuesString() string {
+	return `{
+		"type": "CUSTOM",
+		"profile": "PARTNER",
+		"objectType": "KalturaUserRole"
+	  }`
 }
--- tests/ott/go/test/error_test.go
+++ tests/ott/go/test/error_test.go
@@ -2,9 +2,13 @@ package test
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 
+	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient"
 	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/errors"
+	apierrors "github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/errors"
+	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/services"
 	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/test/utils"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,9 +19,43 @@ func TestErrorWithArgs(t *testing.T) {
 
 func TestErrorLogin(t *testing.T) {
 	ctx := utils.WithRequestId(context.Background(), "requestId")
-	_, ks, err := login(ctx, "nonexistingusername", "nopassword")
+	config := kalturaclient.Configuration{
+		ServiceUrl:                "test.com",
+		TimeoutMs:                 30000,
+		MaxConnectionsPerHost:     1024,
+		IdleConnectionTimeoutMs:   30000,
+		MaxIdleConnections:        1024,
+		MaxIdleConnectionsPerHost: 1024,
+	}
+	mockHttpClient := utils.NewMockHttpClient()
+	httpHandler := kalturaclient.NewHttpHandler(kalturaclient.GetBaseUrl(config), mockHttpClient)
+	var client *kalturaclient.Client
+	client = kalturaclient.NewClient(httpHandler.Execute)
+	username := "nonexistingusername"
+	password := "nopassword"
+	mockHttpClient.SetResponse("/api_v3/service/ottUser/action/login", loginErrorResponseFromPhoenix(), 200)
+	loginResponse, err := services.NewOttUserService(client).Login(ctx, partnerId, &username, &password, nil, nil)
+	var ks string
+	if loginResponse != nil {
+		ks = loginResponse.LoginSession.Ks
+	}
 	assert.Error(t, err)
 	// TODO - i want to do this option without casting
 	assert.Equal(t, (err).(*errors.APIException).Code, errors.WrongPasswordOrUserName)
 	assert.Empty(t, ks)
+}
+
+func loginErrorResponseFromPhoenix() []byte {
+	var result struct {
+		Result (struct {
+			Error *apierrors.APIException `json:"error"`
+		}) `json:"result"`
+	}
+	apiException := apierrors.APIException{
+		Code:    "1011",
+		Message: "The username or password is not correct",
+	}
+	result.Result.Error = &apiException
+	resultBytes, _ := json.Marshal(result)
+	return resultBytes
 }
