9c1047b2351cd6eb63d7c98e4503dc3d11405033
shir.bruchim <shir.bruchim@kaltura.com>
2021-04-08 16:21:01 +0300

    Poly tests

--- tests/ott/go/test/polymorphism_test.go
+++ tests/ott/go/test/polymorphism_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"encoding/json"
+	"regexp"
 	"testing"
 
 	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/channelorderby"
@@ -12,11 +13,7 @@ import (
 
 func TestPolySerialization(t *testing.T) {
 	t.Parallel()
-
-	channelFilter := &types.ChannelFilter{
-		OrderBy:      string(channelorderby.NAME_ASC),
-		GroupOrderBy: groupbyorder.COUNT_DESC,
-	}
+	channelFilter := GetChannelFilter()
 	_ = GetAssets(t, channelFilter)
 }
 
@@ -24,24 +21,89 @@ func GetAssets(t *testing.T, filter types.AssetFilterInterface) []types.AssetCon
 	// request
 	bytes, err := json.Marshal(&filter)
 	assert.NoError(t, err)
-	t.Log(string(bytes))
-	assert.Equal(t, `{"name":"","dynamicOrderBy":{"name":"","orderBy":"","objectType":"KalturaDynamicOrderBy"},"kSql":"","groupBy":null,"groupOrderBy":"count_desc","idEqual":0,"excludeWatched":false,"orderBy":"NAME_ASC","objectType":"KalturaChannelFilter"}`, string(bytes))
+	space := regexp.MustCompile(`\s+`)
+	channelFilterString := space.ReplaceAllString(GetChannelFilterString(), "")
+	assert.Equal(t, channelFilterString, string(bytes))
 
 	// response
-	response := []byte(`{ "totalCount":2, "objects": [{"objectType":"KalturaProgramAsset", "id": 1, "epgId": "1b"},{"objectType":"KalturaMediaAsset", "id": 2, "EntryId":"a3"}]}`)
+	assetListResponseString := space.ReplaceAllString(GetAssetListResponseString(), "")
+	response := []byte(assetListResponseString)
 	var assetListResponse types.AssetListResponse
 	err = json.Unmarshal(response, &assetListResponse)
 	assert.NoError(t, err)
 
-	objects := assetListResponse.Objects
-	assert.Equal(t, 2, len(objects))
-	objects[0].Get().GetId()
+	assert.Equal(t, 2, len(assetListResponse.Objects))
+	mediaAsset := GetMediaAsset()
+	assert.Equal(t, mediaAsset, assetListResponse.Objects[0].MediaAsset)
+	assert.Equal(t, mediaAsset, assetListResponse.Objects[0].Get())
+	liveAsset := GeLiveAsset()
+	assert.Equal(t, liveAsset, assetListResponse.Objects[1].LiveAsset)
+	assert.Equal(t, liveAsset, assetListResponse.Objects[1].Get())
+	//TODO AMIT - ASSERT ALL TYPES
+	return assetListResponse.Objects
+}
+
+func GetChannelFilter() *types.ChannelFilter {
+	return &types.ChannelFilter{
+		OrderBy: string(channelorderby.NAME_ASC), // prop from Filter
+		Name:    "NameTest",                      // prop from PersistedFilter
+		DynamicOrderBy: types.DynamicOrderBy{ // prop from AssetFilter
+			Name: "DynamicOrderByNameTest",
+		},
+		GroupOrderBy:   groupbyorder.COUNT_DESC, // prop from BaseSearchAssetFilter
+		ExcludeWatched: true,                    // prop from ChannelFilter
+	}
+}
+
+func GetMediaAsset() *types.MediaAsset {
 	helperId1 := int64(1)
-	helperEpgId := "1b"
+	return &types.MediaAsset{
+		Id:      &helperId1,         // prop from Asset
+		EntryId: "MediaEntryIdTest", // prop from MediaAsset
+	}
+}
+
+func GeLiveAsset() *types.LiveAsset {
 	helperId2 := int64(2)
+	return &types.LiveAsset{
+		Id:             &helperId2,           // prop from Asset
+		EntryId:        "LiveEntryIdTest",    // prop from MediaAsset
+		ExternalCdvrId: "ExternalCdvrIdTest", // prop from LiveAsset
+	}
+}
 
-	assert.Equal(t, &types.ProgramAsset{Id: &helperId1, EpgId: &helperEpgId}, objects[0].ProgramAsset)
-	assert.Equal(t, &types.MediaAsset{Id: &helperId2, EntryId: "a3"}, objects[1].MediaAsset)
+//TODO AMIT - ProgramAsset, RecordingAsset, Epg - set one property from each inheritence
 
-	return objects
+func GetChannelFilterString() string {
+	return `{
+		"excludeWatched": true,
+		"groupOrderBy": "count_desc",
+		"dynamicOrderBy": {
+			"name": "DynamicOrderByNameTest",
+			"objectType": "KalturaDynamicOrderBy"
+		},
+		"name": "NameTest",
+		"orderBy": "NAME_ASC",
+		"objectType": "KalturaChannelFilter"
+	  }`
+}
+
+// TODO AMIT - ADD VALUES TO RESPONSE STRING
+func GetAssetListResponseString() string {
+	return `{
+		"totalCount": 2,
+		"objects": [
+		  {
+			"objectType": "KalturaMediaAsset",
+			"id": 1,
+			"entryId": "MediaEntryIdTest"
+		  },
+		  {
+			"objectType": "KalturaLiveAsset",
+			"id": 2,
+			"entryId": "LiveEntryIdTest",
+			"externalCdvrId": "ExternalCdvrIdTest"
+		  }
+		]
+	  }`
 }
