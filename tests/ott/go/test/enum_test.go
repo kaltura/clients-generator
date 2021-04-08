2c9eb62eb33de224fc9c4834e14898e6070f4219
KALTURA\amit.shkoory <amit.shkoory@kaltura.com>
2021-04-08 15:36:49 +0300

    BEO-9733_Go_Client_Libs-enumtest

--- tests/ott/go/test/enum_test.go
+++ tests/ott/go/test/enum_test.go
@@ -3,14 +3,19 @@ package test
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/metaorderby"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/epgorderby"
+	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/metaorderby"
+	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/enums/ruleActionType"
+	"github.com/kaltura/KalturaOttGeneratedAPIClientsGo/kalturaclient/types/"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestSerialization(t *testing.T) {
+func TestSerializationWithoutValues(t *testing.T) {
 	t.Parallel()
 	order := metaorderby.NAME_ASC
+
 	var testCases = []SomeStruct{
 		{OrderBy: &order},
 		{OrderBy: nil},
@@ -22,6 +27,28 @@ func TestSerialization(t *testing.T) {
 			t.Log(string(bytes))
 
 			after := SomeStruct{}
+			err = json.Unmarshal(bytes, &after)
+			assert.NoError(t, err)
+
+			assert.Equal(t, before, after)
+		})
+	}
+}
+
+func TestSerializationOfValues(t *testing.T) {
+	t.Parallel()
+	order := ruleActionType.START_DATE_OFFSET
+
+	var testCases = []types.AccessControlBlockAction{
+		{OrderBy: &order},
+	}
+	for _, before := range testCases {
+		t.Run(fmt.Sprintf("order: %v", before.OrderBy), func(t *testing.T) {
+			bytes, err := json.Marshal(before)
+			assert.NoError(t, err)
+			t.Log(string(bytes))
+
+			after := types.AccessControlBlockAction{}
 			err = json.Unmarshal(bytes, &after)
 			assert.NoError(t, err)
 
@@ -42,6 +69,10 @@ func TestDeserialization(t *testing.T) {
 	assert.Equal(t, SomeStruct{}, deserialized)
 }
 
+// epgcache
+// type SomeStruct struct {
+// 	OrderBy *epgorderby.EpgOrderBy`json:"orderBy,omitempty"`
+// }
 type SomeStruct struct {
-	OrderBy *metaorderby.MetaOrderBy `json:"orderBy,omitempty"`
+	OrderBy *epgorderby.EpgOrderBy `json:"orderBy,omitempty"`
 }
