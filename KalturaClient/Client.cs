730f6f45989d232ccebfb573aa44bc0ea54825e5
Backend CI <ott.rnd.core@kaltura.com>
2021-04-18 11:07:00 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/434, branch master

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -36,7 +36,7 @@ namespace Kaltura
 		public Client(Configuration config) : base(config)
 		{
 				ApiVersion = "6.3.0.29045";
-				ClientTag = "dotnet:21-04-16";
+				ClientTag = "dotnet:21-04-18";
 		}
 	
 		#region Properties
