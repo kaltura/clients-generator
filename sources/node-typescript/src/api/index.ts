2ec1e59c4791fb4de4d65f10625360fbd0e1c32d
Daniel Schwartz <daniel.schwartz@kaltura.com>
2021-10-31 16:48:03 +0200

    feat: added Node Typescript client
    * library is called: kaltura-node-typescript-client
    * just took Typescript client and basically changed only:
      `node-typescript\src\adapters\utils.ts`  `createCancelableAction()` function
      to use got library instead of xmlHttpRequest.
    * Note: I still compile TS with "DOM" since I kept upload form data
      functionallity that uses File and FormData objects.. wil be removed in the future
      to support upload stuff.
    * So most new changes aree in config/ and lib/ (or everythign but sources/node-typescript)
    * Copied test (ovp only lib, no ott)
    * Using [debug](https://www.npmjs.com/package/debug) for logging
    * **library has "got" and "debug" as dependencies (not peer dependencies)**

---
+++ sources/node-typescript/src/api/index.ts
@@ -0,0 +1,10 @@
+export { KalturaClientException } from './kaltura-client-exception';
+export { KalturaAPIException } from './kaltura-api-exception';
+export { KalturaMultiRequest } from './kaltura-multi-request';
+export { KalturaMultiResponse } from './kaltura-multi-response';
+export { KalturaRequest } from './kaltura-request';
+export { KalturaRequestBase } from './kaltura-request-base';
+export { KalturaUploadRequest } from './kaltura-upload-request';
+export { KalturaResponse } from './kaltura-response';
+export { KalturaTypesFactory } from './kaltura-types-factory';
+export * from './kaltura-request-options';
---
+++ sources/node-typescript/src/index.ts
@@ -0,0 +1,4 @@
+export * from './api/index';
+export * from './cancelable-action';
+export * from './kaltura-client-options';
+export * from './kaltura-client-service';
