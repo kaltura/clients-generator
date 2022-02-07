907bb125052994e2a48dac0733ed36851fadc152
Jess Portnoy <jess.portnoy@kaltura.com>
2022-02-07 15:46:20 +0000

    Reinstate placeholder.

--- sources/python/KalturaClient/Client.py
+++ sources/python/KalturaClient/Client.py
@@ -117,7 +117,7 @@ class KalturaClient(object):
         self.callsQueue = []
         self.requestHeaders = {}
         self.clientConfiguration = {
-            'clientTag': 'python-22-01-10',
+            'clientTag': 'python-@DATE@',
             'apiVersion': API_VERSION,
         }
         self.requestConfiguration = {}
