2bb831c01c98991d77c85f7640d56ba66470b8cb
Jess Portnoy <jess.portnoy@kaltura.com>
2022-03-16 16:16:19 +0000

    Revert "push https://github.com/kaltura/clients-generator/pull/769/files to 18.2.0."

--- sources/python/KalturaClient/Client.py
+++ sources/python/KalturaClient/Client.py
@@ -450,7 +450,8 @@ class KalturaClient(object):
                         expiry=86400, privileges=''):
         rand = random.randint(0, 0x10000)
         expiry = int(time.time()) + expiry
-        fields = [partnerId, expiry, type_, rand, userId, privileges]
+        fields = [
+            partnerId, partnerId, expiry, type_, rand, userId, privileges]
         fields = [
             x if isinstance(x, six.binary_type)
             else six.text_type(x).encode(KalturaClient.ENCODING)
