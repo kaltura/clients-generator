888cf1a39fdf8b13def6e980a32a242ce1a9cd1b
atarsh <atar.shadmi@kaltura.com>
2023-05-04 13:47:12 +0300

    KMS-27368 parallel chunks, naive

---
+++ sources/typescript/src/adapters/kaltura-upload-connections-manager.ts
@@ -0,0 +1,67 @@
+export class KalturaUploadConnectionsManager {
+     private static _availableConnections: number;
+     private static _callbacks: (() => void)[];
+     public static _totalConnections: number = -1; // -1 as in not initialized
+
+    public static setTotalConnections(n: number): void {
+        // only if not yet initialized
+        if (this._totalConnections === -1) {
+            this._totalConnections = n;
+            this._availableConnections = n;
+            // this is a fine opportunity to initialize this as well:
+            this._callbacks = [];
+        }
+    }
+
+    public static getTotalConnections(): number {
+        return this._totalConnections;
+    }
+
+    /**
+     * let an adapter use a connection
+     * @return true if there is an available connection, false otherwise
+     */
+    public static retrieveConnection(): boolean {
+        if (this._availableConnections >= 1) {
+            this._availableConnections -= 1;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * return a connection to the pool
+     */
+    public static releaseConnection(): void {
+        this._availableConnections += 1;
+        if (this._availableConnections === 1) {
+            // if we got here, previously there were no available connections,
+            // and now there is now - notify the first item in the queue and remove it.
+            const cb = this._callbacks.shift();
+            cb();
+        }
+    }
+
+    /**
+     * request to be notified when a connection is available.
+     * callback will be automatically removed from queue once triggered.
+     * @param callback
+     */
+    public static addAvailableConnectionsCallback(callback: () => void): void {
+        this._callbacks.push(callback);
+    }
+
+    /**
+     * stop waiting for free connections
+     * @param callback
+     */
+    public static removeAvailableConnectionsCallback(callback: () => void): void {
+        for (let i = 0; this._callbacks.length; i++) {
+            if (this._callbacks[i] === callback) {
+                this._callbacks.splice(i, 1);
+                break;
+            }
+        }
+    }
+
+}
--- sources/typescript/src/adapters/kaltura-upload-request-adapter.ts
+++ sources/typescript/src/adapters/kaltura-upload-request-adapter.ts
@@ -5,15 +5,46 @@ import { KalturaRequestOptions } from "../api/kaltura-request-options";
 import { KalturaClientOptions } from "../kaltura-client-options";
 import { KalturaAPIException } from "../api/kaltura-api-exception";
 import { CancelableAction } from "../cancelable-action";
+import { KalturaUploadConnectionsManager } from "./kaltura-upload-connections-manager";
 
-interface UploadByChunksData {
+interface UploadBySequentialChunksData {
     enabled: boolean;
     resume: boolean;
     resumeAt: number;
     finalChunk: boolean;
 }
 
+interface UploadByParallelChunksData {
+    chunkUploadEnabled: boolean;
+
+    /**
+     * bytes uploaded so far
+     */
+    loaded: number;
+
+    /**
+     * total number of chunks in the current file
+     */
+    totalChunks: number;
+
+    /**
+     * number of bytes per chunk
+     */
+    chunkSize: number;
+
+    /**
+     * number of chunks done uploading
+     */
+    chunksUploaded: number;
+
+    /**
+     * number of the next chunk to start uploading
+     */
+    nextChunkIndex: number;
+}
+
 export class KalturaUploadRequestAdapter {
+
     private _chunkUploadSupported(request: KalturaUploadRequest<any>): boolean {
         // SUPPORTED BY BROWSER?
         // Check if these features are support by the browser:
@@ -37,17 +68,30 @@ export class KalturaUploadRequestAdapter {
     }
 
     constructor(public clientOptions: KalturaClientOptions, public defaultRequestOptions: KalturaRequestOptions) {
-
+        if (clientOptions.parallelUploadsDisabled) {
+            return;
+        }
+        if (KalturaUploadConnectionsManager.getTotalConnections() === -1) {
+            // initialize manager
+            KalturaUploadConnectionsManager.setTotalConnections(clientOptions.maxConcurrentUploadConnections || 6);
+        }
     }
 
     transmit(request: KalturaUploadRequest<any>): CancelableAction<any> {
+        if (this.clientOptions.parallelUploadsDisabled) {
+            return this.transmit_sequential(request);
+        }
+        return this.transmit_parallel(request);
+    }
+
+    transmit_sequential(request: KalturaUploadRequest<any>): CancelableAction<any> {
         return new CancelableAction((resolve, reject, action) => {
             const uploadedFileSize = !isNaN(request.uploadedFileSize) && isFinite(request.uploadedFileSize) && request.uploadedFileSize > 0 ? request.uploadedFileSize : 0;
-            const data: UploadByChunksData = {
+            const data: UploadBySequentialChunksData = {
                 enabled: this._chunkUploadSupported(request),
                 resume: !!uploadedFileSize,
                 finalChunk: false,
-                resumeAt: uploadedFileSize
+                resumeAt: uploadedFileSize,
             };
 
             let activeAction: CancelableAction<any>;
@@ -60,24 +104,7 @@ export class KalturaUploadRequestAdapter {
             const handleChunkUploadSuccess = result => {
                 if (!data.enabled || data.finalChunk) {
                     activeAction = null;
-
-                    try {
-                        const response = request.handleResponse(result);
-
-                        if (response.error) {
-                            reject(response.error);
-                        } else {
-                            resolve(response.result);
-                        }
-                    } catch (error) {
-                        if (error instanceof KalturaClientException || error instanceof KalturaAPIException) {
-                            reject(error);
-                        } else {
-                            const errorMessage = error instanceof Error ? error.message : typeof error === "string" ? error : null;
-                            reject(new KalturaClientException("client::response-unknown-error", errorMessage || "Failed to parse response"));
-                        }
-                    }
-
+                    this._handleFinalChunkResponse(request, result, reject, resolve);
 
                 } else {
                     activeAction = this._chunkUpload(request, data).then(handleChunkUploadSuccess, handleChunkUploadError);
@@ -97,6 +124,109 @@ export class KalturaUploadRequestAdapter {
         });
     }
 
+    transmit_parallel(request: KalturaUploadRequest<any>): CancelableAction<any> {
+        return new CancelableAction((resolve, reject, action) => {
+            const { file} = request.getFileInfo();
+            const chunkSize = this._getChunkSize();
+
+            const data: UploadByParallelChunksData = {
+                chunkUploadEnabled: this._chunkUploadSupported(request),
+                loaded: 0,
+                totalChunks: Math.ceil(file.size / chunkSize),
+                chunkSize: chunkSize,
+                chunksUploaded: 0,
+                nextChunkIndex: 0,
+            };
+
+            let activeAction: CancelableAction<any>;
+
+            const tryUpload = (waitIfNoConnections = true) => {
+                if (KalturaUploadConnectionsManager.retrieveConnection()) {
+                    console.log("tryUpload - got connection");
+                    activeAction = this._uploadChunk(request, data, data.nextChunkIndex)
+                        .then(handleChunkUploadSuccess, handleChunkUploadError);
+                    data.nextChunkIndex += 1;
+                    return true;
+                }
+                else if (waitIfNoConnections) {
+                    console.log("tryUpload - no connections, waiting");
+                    KalturaUploadConnectionsManager.addAvailableConnectionsCallback(waitForConnection);
+                }
+                return false;
+            };
+
+            const handleChunkUploadError = reason => {
+                activeAction = null;
+                reject(reason);
+            };
+
+            const handleChunkUploadSuccess = result => {
+                console.log("handleChunkUploadSuccess");
+                // "clean up":
+                activeAction = null;
+                data.chunksUploaded += 1;
+                KalturaUploadConnectionsManager.releaseConnection();
+
+                // was this the final chunk?
+                const { file} = request.getFileInfo();
+                const chunkSize = this._getChunkSize();
+                const totalChunks = Math.ceil(file.size / chunkSize);
+                const finalChunk = data.chunksUploaded >= totalChunks;
+                const hasMoreChunks = data.nextChunkIndex < totalChunks;
+
+                if (!data.chunkUploadEnabled || finalChunk) {
+                    this._handleFinalChunkResponse(request, result, reject, resolve);
+                }
+                else if (hasMoreChunks) {
+                    tryUpload();
+                }
+            };
+
+            const waitForConnection = () => {
+                if (KalturaUploadConnectionsManager.retrieveConnection()) {
+                    activeAction = this._uploadChunk(request, data, data.nextChunkIndex)
+                        .then(handleChunkUploadSuccess, handleChunkUploadError);
+                    data.nextChunkIndex += 1;
+                }
+            };
+
+            while (true) {
+                if (data.nextChunkIndex >= data.totalChunks || !tryUpload(false)) {
+                    break;
+                }
+                // add as many chunks as possible
+                console.log("chunk added");
+            }
+
+            return () => {
+                if (activeAction) {
+                    activeAction.cancel();
+                    activeAction = null;
+                }
+            };
+        });
+    }
+
+    private _handleFinalChunkResponse(request: KalturaUploadRequest<any>, result, reject: (reason: Error) => any, resolve: (value: any) => any) {
+        console.log("_handleFinalChunkResponse");
+        try {
+            const response = request.handleResponse(result);
+
+            if (response.error) {
+                reject(response.error);
+            } else {
+                resolve(response.result);
+            }
+        } catch (error) {
+            if (error instanceof KalturaClientException || error instanceof KalturaAPIException) {
+                reject(error);
+            } else {
+                const errorMessage = error instanceof Error ? error.message : typeof error === "string" ? error : null;
+                reject(new KalturaClientException("client::response-unknown-error", errorMessage || "Failed to parse response"));
+            }
+        }
+    }
+
     private _getFormData(filePropertyName: string, fileName: string, fileChunk: File | Blob): FormData {
         const result = new FormData();
         result.append("fileName", fileName);
@@ -104,7 +234,13 @@ export class KalturaUploadRequestAdapter {
         return result;
     }
 
-    private _chunkUpload(request: KalturaUploadRequest<any>, uploadChunkData: UploadByChunksData): CancelableAction<any> {
+    /**
+     * chunk upload for sequential chunks upload
+     * @param request
+     * @param uploadChunkData
+     * @private
+     */
+    private _chunkUpload(request: KalturaUploadRequest<any>, uploadChunkData: UploadBySequentialChunksData): CancelableAction<any> {
         return new CancelableAction((resolve, reject) => {
             const parameters = prepareParameters(request, this.clientOptions, this.defaultRequestOptions);
 
@@ -115,21 +251,7 @@ export class KalturaUploadRequestAdapter {
             let fileStart = 0;
 
             if (uploadChunkData.enabled) {
-                let actualChunkFileSize: number = null;
-                const userChunkFileSize = this.clientOptions ? this.clientOptions.chunkFileSize : null;
-
-                if (userChunkFileSize && Number.isFinite(userChunkFileSize) && !Number.isNaN(userChunkFileSize)) {
-                    if (userChunkFileSize < 1e5) {
-                        console.warn(`user requested for invalid upload chunk size '${userChunkFileSize}'. minimal value 100Kb. using minimal value 100Kb instead`);
-                        actualChunkFileSize = 1e5;
-                    } else {
-                        console.log(`using user requetsed chunk size '${userChunkFileSize}'`);
-                        actualChunkFileSize = userChunkFileSize;
-                    }
-                } else {
-                    console.log(`using default chunk size 5Mb`);
-                    actualChunkFileSize = 5e6; // default
-                }
+                let actualChunkFileSize: number = this._getChunkSize();
 
                 uploadChunkData.finalChunk = (file.size - uploadChunkData.resumeAt) <= actualChunkFileSize;
 
@@ -211,5 +333,119 @@ export class KalturaUploadRequestAdapter {
                 }
             };
         });
+    }
+
+    /**
+     * chunk upload for parallel chunks upload
+     * @param request
+     * @param uploadChunkData
+     * @param chunkIndex
+     * @private
+     */
+    private _uploadChunk(request: KalturaUploadRequest<any>, uploadChunkData: UploadByParallelChunksData, chunkIndex: number): CancelableAction<any> {
+        console.log("_uploadChunk, chunkIndex ", chunkIndex);
+        return new CancelableAction((resolve, reject) => {
+            const parameters = prepareParameters(request, this.clientOptions, this.defaultRequestOptions);
+
+            let isComplete = false;
+            const {propertyName, file} = request.getFileInfo();
+            let data = this._getFormData(propertyName, file.name, file);
+
+            let fileStart = 0;
+
+            if (uploadChunkData.chunkUploadEnabled) {
+                const chunkSize = uploadChunkData.chunkSize;
+                const resumeAt = chunkIndex * chunkSize;
+                const finalChunk = (file.size - resumeAt) <= chunkSize;
+
+                fileStart = resumeAt;
+                const fileEnd = finalChunk ? file.size : fileStart + chunkSize;
+
+                data = this._getFormData(propertyName, file.name, file.slice(fileStart, fileEnd, file.type));
+
+                parameters.resume = chunkIndex > 0;
+                parameters.resumeAt = resumeAt;
+                parameters.finalChunk = finalChunk;
+            } else {
+                console.log(`chunk upload not supported by browser or by request. Uploading the file as-is`);
+            }
+
+            // const { service, action, ks, ...queryparams } = parameters;
+            const {service, action, ...queryparams} = parameters;
+            const endpointUrl = createEndpoint(request, this.clientOptions, service, action, queryparams);
+
+            const xhr = new XMLHttpRequest();
+
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4) {
+                    if (isComplete) {
+                        return;
+                    }
+                    isComplete = true;
+                    let resp;
+
+                    try {
+                        if (xhr.status === 200) {
+                            resp = JSON.parse(xhr.response);
+                        } else {
+                            resp = new KalturaClientException("client::upload-failure", xhr.responseText || "failed to upload file");
+                        }
+                    } catch (e) {
+                        resp = new KalturaClientException("client::upload-failure", e.message || "failed to upload file");
+                    }
+
+                    if (resp instanceof Error) {
+                        reject(resp);
+                    } else {
+                        if (uploadChunkData.chunkUploadEnabled) {
+                            if (typeof resp.uploadedFileSize === "undefined" || resp.uploadedFileSize === null) {
+                                reject(new KalturaClientException("client::upload-failure", `uploaded chunk of file failed, expected response with property 'uploadedFileSize'`));
+                                return;
+                            }
+                        }
+
+                        resolve(resp);
+                    }
+                }
+            };
+
+            // const progressCallback = request._getProgressCallback();
+            // if (progressCallback) {
+            //     xhr.upload.addEventListener("progress", e => {
+            //         if (e.lengthComputable) {
+            //             progressCallback.apply(request, [e.loaded + fileStart, file.size]);
+            //         } else {
+            //             // Unable to compute progress information since the total size is unknown
+            //         }
+            //     }, false);
+            // }
+
+            xhr.open("POST", endpointUrl);
+            xhr.send(data);
+
+            return () => {
+                if (!isComplete) {
+                    isComplete = true;
+                    xhr.abort();
+                }
+            };
+        });
+    }
+
+    private _getChunkSize() {
+        const userChunkFileSize = this.clientOptions ? this.clientOptions.chunkFileSize : null;
+
+        if (userChunkFileSize && Number.isFinite(userChunkFileSize) && !Number.isNaN(userChunkFileSize)) {
+            if (userChunkFileSize < 1e5) {
+                console.warn(`user requested for invalid upload chunk size '${userChunkFileSize}'. minimal value 100Kb. using minimal value 100Kb instead`);
+                return 1e5;
+            } else {
+                console.log(`using user requested chunk size '${userChunkFileSize}'`);
+                return userChunkFileSize;
+            }
+        } else {
+            console.log(`using default chunk size 5Mb`);
+            return 5e6; // default
+        }
     }
 }
