f77251d55ba9de255f531f3253e36d674815cc2b
Noam Tamim <noam.tamim@kaltura.com>
2023-12-13 20:01:42 +0000

    Cleanups

--- gen-ovp.sh
+++ gen-ovp.sh
@@ -1,15 +1,19 @@
 #!/bin/bash -e
 
 # Make sure we're running from the root of the repo
-cd $(dirname $0)/../..
+cd $(dirname $0)
 
 # Input env vars:
+workDir="$PWD/temp"
 client_name=all
-outdir=$PWD/web/content
+outdir=$workDir/web/content
 distdir=$PWD/dist
 
 schema_url="https://www.kaltura.com/api_v3/api_schema.php"
-clientsList='ajax android as3FlexClient bpmn cli flex35 java js ngx node nodePlayServer nodeTypescript php5 php53 php5Zend pojoOld python swift typescript'
+clientsList='ajax android as3FlexClient cli csharp flex35 go java js ngx node nodeTypescript objc php53 php5Zend python ruby rxjs swift typescript'
+
+# php5 has issues
+
 
 # Prepare Clients List
 clientName=$client_name
@@ -19,7 +23,6 @@ fi
 
 # Generate Clients
 clients="$clientsList"
-workDir="$PWD"
 
 colorDefault="\033[0m"
 colorPurple="\033[0;35m"
@@ -27,31 +30,27 @@ colorGreen="\033[0;32m"
 
 echo -e "${colorGreen}Creating required folders ..."
 mkdir -p $outdir/clientlibs
-mkdir -p $outdir/temp
 
 echo -e "${colorGreen}Getting XML ..."
 curl -o "$outdir/clientlibs/KalturaClient.xml" "$schema_url"
+
+rm -rf "$distdir"
+mkdir -p "$distdir"
 
 for generateClient in $clients
 do
     echo -e "-----------------------------------------------\n"
     echo -e "${colorPurple}Generating client: $generateClient"
-    rm -rf $outdir/temp/$generateClient
     mkdir -p $outdir/clientlibs/$generateClient
     echo -e "$colorDefault"
-    php exec.php -rserver -x$outdir/clientlibs/KalturaClient.xml $generateClient $outdir/clientlibs
+    php exec.php -x$outdir/clientlibs/KalturaClient.xml $generateClient $outdir/clientlibs
     if [ $? -eq 1 ]
     then    
         echo "Error Generating client $generateClient"
         echo "exit 1"
     fi
-    mv -f $outdir/clientlibs/$generateClient $outdir/temp/
-    # tar czf $outdir/temp/$generateClient.tar.gz -C $outdir/temp/$generateClient .
+    mv -f $outdir/clientlibs/$generateClient $distdir
 done
 
-echo -e "${colorGreen}Copying generated clients ..."
-rm -rf "$distdir"
-mkdir -p "$distdir"
-mv -f $outdir/temp/* "$distdir"
 mv -f $outdir/clientlibs/KalturaClient.xml "$distdir"
-rm -rf server web
+rm -rf $workDir
