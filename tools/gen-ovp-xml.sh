5a8379e5db15e68ce2438a215c845ea6ea11bc26
Noam Tamim <git@noamtamim.com>
2024-03-10 13:43:46 +0200

    Try to download a file without GH CLI

--- tools/gen-ovp-xml.sh
+++ tools/gen-ovp-xml.sh
@@ -1,8 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 # Required env vars:
 # SERVER_BRANCH
 # GH_TOKEN or GITHUB_TOKEN that has read access to server-saas-config repo
+
+# Required tools:
+# php 7.2
+# GitHub CLI (gh)
+# jq
+# git
+# GNU sed 
 
 cd $(dirname $0)/..
 
@@ -15,13 +22,17 @@ cd server-temp
 git clone https://github.com/kaltura/server --depth=1 --branch=$SERVER_BRANCH --single-branch
 
 # Download plugins ini from server-saas-config
-gh api 'repos/kaltura/server-saas-config/contents/configurations/plugins.ini.base' --jq '.content | @base64d' > server/configurations/plugins.ini
+curl -O -H 'Authorization: token $GH_TOKEN' \
+  -H 'Accept: application/vnd.github.v3.raw' \
+  -L https://api.github.com/repos/server-saas-config/contents/configurations/plugins.ini.base
+
+# gh api 'repos/kaltura/server-saas-config/contents/configurations/plugins.ini.base' --jq '.content | @base64d' > server/configurations/plugins.ini
 
 # Comment out problematic lines in bootstrap.php
 bootstrapFile="server/api_v3/generator/bootstrap.php"
-sed -i 's/date_default_timezone_set/#date_default_timezone_set/g' $bootstrapFile
-sed -i 's/kLoggerCache/#kLoggerCache/g' $bootstrapFile
-sed -i 's/KalturaLog/#KalturaLog/g' $bootstrapFile
+sed -i 's/date_default_timezone_set/#date_default_timezone_set/g' "$bootstrapFile"
+sed -i 's/kLoggerCache/#kLoggerCache/g' "$bootstrapFile"
+sed -i 's/KalturaLog/#KalturaLog/g' "$bootstrapFile"
 
 # Create required folders
 mkdir -p server/cache/api_v3
